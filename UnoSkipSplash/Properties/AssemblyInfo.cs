6d583682562cc4f67b762273cac69e40a8193513
Usman Shafiq <ushafiq141@gmail.com>
2022-01-23 17:51:30 -0500

    Version Bump

--- UnoSkipSplash/Properties/AssemblyInfo.cs
+++ UnoSkipSplash/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using MelonLoader;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: MelonInfo(typeof(UnoSkipSplash.SkipSplash), "UNO Skip Splash", "1.0.2", "DubyaDude", "https://github.com/DubyaDude/")]
+[assembly: MelonInfo(typeof(UnoSkipSplash.SkipSplash), "UNO Skip Splash", "1.1.0", "DubyaDude", "https://github.com/DubyaDude/")]
 [assembly: MelonGame("Ubisoft", "UNO")]
 [assembly: AssemblyTitle("UnoSkipSplash")]
 [assembly: AssemblyDescription("")]
@@ -35,5 +35,5 @@ using MelonLoader;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
